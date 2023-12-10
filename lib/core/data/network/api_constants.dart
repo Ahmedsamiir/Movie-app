50881b3a4a922d41fd5a8b2d4a17a11ea537d4c8
mac <Ahmedsamir22081997@gmail.com>
2023-12-10 09:06:16 +0400

    refactoring the code

--- lib/core/data/network/api_constants.dart
+++ lib/core/data/network/api_constants.dart
@@ -11,27 +11,20 @@ class ApiConstants {
   static const String baseAvatarUrl = 'https://image.tmdb.org/t/p/w185';
   static const String baseVideoUrl = 'https://www.youtube.com/watch?v=';
 
-  static const String moviePlaceHolder =
-      'https://davidkoepp.com/wp-content/themes/blankslate/images/Movie%20Placeholder.jpg';
+  static const String moviePlaceHolder = 'https://davidkoepp.com/wp-content/themes/blankslate/images/Movie%20Placeholder.jpg';
 
-  static const String castPlaceHolder =
-      'https://palmbayprep.org/wp-content/uploads/2015/09/user-icon-placeholder.png';
+  static const String castPlaceHolder = 'https://palmbayprep.org/wp-content/uploads/2015/09/user-icon-placeholder.png';
 
-  static const String avatarPlaceHolder =
-      'https://cdn.pixabay.com/photo/2018/11/13/21/43/avatar-3814049__480.png';
+  static const String avatarPlaceHolder = 'https://cdn.pixabay.com/photo/2018/11/13/21/43/avatar-3814049__480.png';
 
-  static const String stillPlaceHolder =
-      'https://popcornsg.s3.amazonaws.com/gallery/1577405144-six-year.png';
+  static const String stillPlaceHolder = 'https://popcornsg.s3.amazonaws.com/gallery/1577405144-six-year.png';
 
   // movies paths
-  static const String nowPlayingMoviesPath =
-      '$baseUrl/movie/now_playing?api_key=$apiKey';
+  static const String nowPlayingMoviesPath = '$baseUrl/movie/now_playing?api_key=$apiKey';
 
-  static const String popularMoviesPath =
-      '$baseUrl/movie/popular?api_key=$apiKey';
+  static const String popularMoviesPath = '$baseUrl/movie/popular?api_key=$apiKey';
 
-  static const String topRatedMoviesPath =
-      '$baseUrl/movie/top_rated?api_key=$apiKey';
+  static const String topRatedMoviesPath = '$baseUrl/movie/top_rated?api_key=$apiKey';
 
   static String getMovieDetailsPath(int movieId) {
     return '$baseUrl/movie/$movieId?api_key=$apiKey&append_to_response=videos,credits,reviews,similar';
@@ -46,14 +39,11 @@ class ApiConstants {
   }
 
   // tv shows paths
-  static const String onAirTvShowsPath =
-      '$baseUrl/tv/on_the_air?api_key=$apiKey&with_original_language=en';
+  static const String onAirTvShowsPath = '$baseUrl/tv/on_the_air?api_key=$apiKey&with_original_language=en';
 
-  static const String popularTvShowsPath =
-      '$baseUrl/tv/popular?api_key=$apiKey&with_original_language=en';
+  static const String popularTvShowsPath = '$baseUrl/tv/popular?api_key=$apiKey&with_original_language=en';
 
-  static const String topRatedTvShowsPath =
-      '$baseUrl/tv/top_rated?api_key=$apiKey&with_original_language=en';
+  static const String topRatedTvShowsPath = '$baseUrl/tv/top_rated?api_key=$apiKey&with_original_language=en';
 
   static String getTvShowDetailsPath(int tvShowId) {
     return '$baseUrl/tv/$tvShowId?api_key=$apiKey&append_to_response=similar,videos';
